ddb2e8e25b77707c88b3510150157c1b79e9d327
Cesar-MLW <cesarmlwestphal@gmail.com>
2019-05-12 22:22:57 -0300

    starte requestTest.js file

--- GamificacaoTech/routes/home.ts
+++ GamificacaoTech/routes/home.ts
@@ -13,6 +13,11 @@ router.get('/curso', (req: express.Request, res: express.Response) => {
     res.render('curso', { titulo: 'Gamificação TECH', nome_curso: "Banco de Dados"});//estarei deixando como exemplo a rota curso e junto um arquivo chamado curso.ejs
 });
 
+router.get('/requestTest', (req: express.Request, res: express.Response) => {
+    
+    res.render('requestTest', { titulo: 'Gamificação TECH'});
+});
+
 
 
 
